d51f0a8bad0af5e40275b80efe033381eb94dbb7
ExecutorExe <thepowerofprotoss@gmail.com>
2020-11-18 16:40:27 +0300

    Update joyparser.py

--- joyparser.py
+++ joyparser.py
@@ -390,7 +390,8 @@ def download_images(images, download_path, warn_on=True):
                     raise e
 
         for Im_link in links:  # итерация хуяция
-
+            if warn_on:
+                print("<<files left:", counter, ">>")
             counter = counter - 1
 
             path_FileBaseName = d_path + os.sep + os.path.basename(Im_link)
@@ -398,10 +399,11 @@ def download_images(images, download_path, warn_on=True):
 
             if not os.path.exists(path_FileBaseName):
 
+
                 getimage(Im_link, request, path_FileBaseName)
             else:
                 if warn_on:
-                    print("<<files left:", counter, ">>")
+
                     print("<<!File (" + os.path.basename(Im_link) + ") already existing in dir (" + d_path + ")!>>")
 
     # эта функция интуитивно понятна
