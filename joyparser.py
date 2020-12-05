19feb58031e3a177644767da8fb6f2a60894452f
ExecutorExe <thepowerofprotoss@gmail.com>
2020-12-05 13:20:41 +0300

    Update joyparser.py

--- joyparser.py
+++ joyparser.py
@@ -243,11 +243,9 @@ def parser(page, from_page, until_page=0, posttext=False):
     lenpost = len(keys)
     print(lenpost, len(images))
     keys, indices = np.unique(keys, return_index=True)
-    for i in range(lenpost-1,-1,-1):
+    for i in range(lenpost - 1, -1, -1):
 
         if i not in indices:
-
-
 
             del images[i]
             del tags[i]
