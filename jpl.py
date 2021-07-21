ee6da93e54f574ea77755609a59ca54526297a06
ExecutorExe <thepowerofprotoss@gmail.com>
2021-07-21 19:37:02 +0300

    fix mparser

--- jpl.py
+++ jpl.py
@@ -132,9 +132,20 @@ def mparser(link:str,fromto:list):
     mi = []
     mt = []
     mr = []
+    h = {
+    "Host": "m.reactor.cc",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Accept-Encoding": "gzip, deflate",
+    "Connection": "keep-alive",
+    "Cookie": "",
+    "Upgrade-Insecure-Requests": '1',}
+    h["Cookie"] = info_struct.cookie
+
     for n in range(fromto[0],fromto[1],-1):
 
-        soup = bs(rq.get(link + "/" + str(n)).content, "html.parser")
+        soup = bs(rq.get(link + "/" + str(n),headers=h).content, "html.parser")
         soup = soup.select(".postContainer")
         if not soup:
             return -1
